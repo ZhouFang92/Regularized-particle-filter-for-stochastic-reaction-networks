f73bacfde1762f9014c5de9a6a09efa621f3015a
ZhouFang92 <75120101+ZhouFang92@users.noreply.github.com>
2021-10-13 17:21:01 +0200

    Update artificial_move.m

--- AIC_four_species (by CTMC)/artificial_move.m
+++ AIC_four_species (by CTMC)/artificial_move.m
@@ -14,19 +14,5 @@ for i=1:size(k_in,2)
     for j=3:size(k_in,1)
         k_out(j,i)=k_in(j,i);
     end
-    %a(1)=k_in(1,i)+eta*sqrt(1/M)*randn;
-    %a(2)=k_in(2,i)+eta*sqrt(1/M)*randn;
-    %a(3)=k_in(3,i)+eta*sqrt(1/M)*randn;
-    %a(4)=k_in(4,i)+eta*sqrt(1/M)*randn;
-    %a(5)=k_in(5,i)+eta*sqrt(1/M)*randn;
-   %a(1)=min(a(1),1);
-   %k_out(1,i)=max(a(1),0.1);
-   %a(2)=min(a(2),1);
-   %k_out(2,i)=max(a(2),0.1);
-   %a(3)=min(a(3),1);
-   %k_out(3,i)=max(a(3),0.1);
-   %a(4)=min(a(4),1);
-   %k_out(4,i)=max(a(4),0.1);
-   %a(5)=min(a(5),1);
-   %k_out(5,i)=max(a(5),0.1);
+
 end
